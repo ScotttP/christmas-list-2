2232301e135043a0c57a096bc4996ebc1d10d019
Scott Pribe <Scottpribe@gmail.com>
2020-11-04 21:23:33 -0500

    added link conditional rendering

--- src/gifts.js
+++ src/gifts.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 export default function GiftsCard(props) {
+	function enabledVsDisabled() {
+		if (props.gifts[props.index].link === "") {
+			return <b className="linkToGift">Link Unavailable</b>;
+		} else {
+			return (
+				<a
+					className="linkToGift"
+					href={props.gifts[props.index].link}
+					target="_blank"
+					rel="noreferrer"
+					onClick={(e) => e.preventDefault()}
+				>
+					<b>Link to Gift</b>
+				</a>
+			);
+		}
+	}
+
 	return (
 		<div className="giftCards" id={props.gifts[props.index].name + "-Card"}>
 			<img
@@ -12,14 +30,8 @@ export default function GiftsCard(props) {
 			<h4 className="giftName">{props.gifts[props.index].name}</h4>
 			<h4 className="giftPrice">{props.gifts[props.index].price}</h4>
 			<p className="giftNotes">{props.gifts[props.index].notes}</p>
-			<a
-				className="linkToGift"
-				href={props.gifts[props.index].link}
-				target="_blank"
-				rel="noreferrer"
-			>
-				<b>Link to Gift</b>
-			</a>
+			{/*  */}
+			{enabledVsDisabled()}
 		</div>
 	);
 }
